84e2b5be4b14036d88c40a1c5ef225545357f6dd
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-04-06 01:11:26 +0000

    adding main function

---
+++ Forecasting_Traffic_Flow/Ensemble_Learning/Run_Ensemble_Help_Funs.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+
+
+def Run_Ensemble(train, test, model):
+    
+    """
+    This function 
+    """
+    
+    
