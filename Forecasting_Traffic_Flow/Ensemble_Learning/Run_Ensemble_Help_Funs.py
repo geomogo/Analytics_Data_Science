2a31297d8bf5c397bda020fe6429c35e1d57f1aa
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-04-06 01:15:21 +0000

    adding function description

--- Forecasting_Traffic_Flow/Ensemble_Learning/Run_Ensemble_Help_Funs.py
+++ Forecasting_Traffic_Flow/Ensemble_Learning/Run_Ensemble_Help_Funs.py
@@ -6,7 +6,12 @@ from sklearn.ensemble import RandomForestRegressor
 def Run_Ensemble(train, test, model):
     
     """
-    This function 
+    This function loops through all the directions and locations.
+    With the goal of ensemble the prediction from the four
+    different models that were built. It takes three arguments:
+    train: this is the train data-frame.
+    test: this is the test data-frame. 
+    model: model to be used to ensemble the predictions 
     """
     
     
