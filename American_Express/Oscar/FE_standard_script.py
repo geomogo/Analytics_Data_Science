d454b17f71b2051d68fbd99a3a762a518e614a2e
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-06-27 17:45:15 +0000

    updating

--- American_Express/Oscar/FE_standard_script.py
+++ American_Express/Oscar/FE_standard_script.py
@@ -78,10 +78,10 @@ def summary_stats(x):
     d['D_39_range'] = x['D_39'].max() - x['D_39'].min()
     d['D_39_IQR'] = np.percentile(x['D_39'], 75) - np.percentile(x['D_39'], 25)
 #     d['D_39_negative_count'] = np.sum(x['D_39'] < 0) 
-    d['D_39_positive_count'] = np.sum(x['D_39'] > 0)
+#     d['D_39_positive_count'] = np.sum(x['D_39'] > 0)
     d['D_39_values_above_mean'] = np.sum(x['D_39'] > x['D_39'].mean())
     
-    return pd.Series(d, index = ['D_39_mean', 'D_39_median', 'D_39_min', 'D_39_max', 'D_39_range', 'D_39_IQR', 'D_39_positive_count', 'D_39_values_above_mean'])
+    return pd.Series(d, index = ['D_39_mean', 'D_39_median', 'D_39_min', 'D_39_max', 'D_39_range', 'D_39_IQR', 'D_39_values_above_mean'])
 
 data_out = train_deli.groupby('customer_ID').apply(summary_stats)
 data_out['customer_ID'] = data_out.index
