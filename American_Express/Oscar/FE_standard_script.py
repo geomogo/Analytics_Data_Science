e4788b43ef8ce54c6d0fce37f4e5d6d127c1cb8e
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-06-27 15:52:31 +0000

    adding first to last month change

--- American_Express/Oscar/FE_standard_script.py
+++ American_Express/Oscar/FE_standard_script.py
@@ -19,8 +19,8 @@ file_object_2 = bucket_object_2.get()
 file_content_stream_2 = file_object_2.get('Body')
 
 ## Creating data-type dictionary for reading the train data-frame
-dtype_dict = {'customer_ID': "object", 'S_2': "object", 'P_2': 'float16', 'D_41': 'float16', 'B_1': 'float16','B_2': 'float16',
-              'R_1': 'float16','S_3': 'float16','D_41': 'float16','B_3': 'float16','D_42': 'float16','D_43': 'float16','D_44': 'float16',
+dtype_dict = {'customer_ID': "object", 'S_2': "object", 'P_2': 'float16', 'D_39': 'float16', 'B_1': 'float16','B_2': 'float16',
+              'R_1': 'float16','S_3': 'float16','D_39': 'float16','B_3': 'float16','D_42': 'float16','D_43': 'float16','D_44': 'float16',
               'B_4': 'float16','D_45': 'float16','B_5': 'float16','R_2': 'float16','D_46': 'float16','D_47': 'float16','D_48': 'float16',
               'D_49': 'float16','B_6': 'float16','B_7': 'float16','B_8': 'float16','D_50': 'float16','D_51': 'float16','B_9': 'float16',
               'R_3': 'float16','D_52': 'float16','P_3': 'float16','B_10': 'float16','D_53': 'float16','S_5': 'float16','B_11': 'float16',
@@ -40,10 +40,10 @@ dtype_dict = {'customer_ID': "object", 'S_2': "object", 'P_2': 'float16', 'D_41'
               'D_93': 'float16','D_94': 'float16','R_24': 'float16','R_25': 'float16','D_96': 'float16','S_22': 'float16','S_23': 'float16',
               'S_24': 'float16','S_25': 'float16','S_26': 'float16','D_102': 'float16','D_103': 'float16','D_104': 'float16','D_105': 'float16',
               'D_106': 'float16','D_107': 'float16','B_36': 'float16','B_37': 'float16', 'R_26': 'float16','R_27': 'float16','B_38': 'float16',
-              'D_108': 'float16','D_109': 'float16','D_110': 'float16','D_111': 'float16','B_41': 'float16','D_112': 'float16','B_40': 'float16',
+              'D_108': 'float16','D_109': 'float16','D_110': 'float16','D_111': 'float16','B_39': 'float16','D_112': 'float16','B_40': 'float16',
               'S_27': 'float16','D_113': 'float16','D_114': 'float16','D_115': 'float16','D_116': 'float16','D_117': 'float16','D_118': 'float16',
               'D_119': 'float16','D_120': 'float16','D_121': 'float16','D_122': 'float16','D_123': 'float16','D_124': 'float16','D_125': 'float16',
-              'D_126': 'float16','D_127': 'float16','D_128': 'float16','D_129': 'float16','B_41': 'float16','B_42': 'float16','D_130': 'float16',
+              'D_126': 'float16','D_127': 'float16','D_128': 'float16','D_129': 'float16','B_39': 'float16','B_42': 'float16','D_130': 'float16',
               'D_131': 'float16','D_132': 'float16','D_133': 'float16','R_28': 'float16','D_134': 'float16','D_135': 'float16','D_136': 'float16',
               'D_137': 'float16','D_138': 'float16','D_139': 'float16','D_140': 'float16','D_141': 'float16','D_142': 'float16','D_143': 'float16',
               'D_144': 'float16','D_145': 'float16'}
@@ -65,26 +65,34 @@ for i in range(2, (len(D_variables) + 2)):
 train_deli = train[to_select]
 
 ## Computing average at the customer level
-data_avg = pd.DataFrame(train_deli.groupby(['customer_ID'])['D_41', 'target'].mean())
+data_avg = pd.DataFrame(train_deli.groupby(['customer_ID'])['D_39', 'target'].mean())
 data_avg['customer_ID'] = data_avg.index
 data_avg = data_avg.reset_index(drop = True)
-data_avg = data_avg[['customer_ID', 'target', 'D_41']]
-data_avg.columns = ['customer_ID', 'target', 'D_41_mean']
+data_avg = data_avg[['customer_ID', 'target', 'D_39']]
+data_avg.columns = ['customer_ID', 'target', 'D_39_mean']
 
 ## Computing median at the customer level
-data_median = pd.DataFrame(train_deli.groupby(['customer_ID'])['D_41'].median())
+data_median = pd.DataFrame(train_deli.groupby(['customer_ID'])['D_39'].median())
 data_median['customer_ID'] = data_median.index
 data_median = data_median.reset_index(drop = True)
-data_median.columns = ['D_41_median', 'customer_ID']
+data_median.columns = ['D_39_median', 'customer_ID']
 
 ## Computing average change at the customer level
-data_change = pd.DataFrame(train_deli.groupby(['customer_ID'])['D_41'].apply(lambda x: pd.Series(x.to_list()).pct_change().mean()))
+data_change = pd.DataFrame(train_deli.groupby(['customer_ID'])['D_39'].apply(lambda x: pd.Series(x.to_list()).pct_change().mean()))
 data_change['customer_ID'] = data_change.index
 data_change = data_change.reset_index(drop = True)
-data_change.columns = ['D_41_change', 'customer_ID']
+data_change.columns = ['D_39_change', 'customer_ID']
+
+## Computing change from first to last month
+data_change_first_last = pd.DataFrame(train_deli.groupby(['customer_ID'])['D_39'].apply(lambda x: pd.Series(x..iloc[[0, -1]].to_list()).pct_change())).unstack()
+data_change_first_last = data_change_first_last.drop(columns = ('D_39', 0), axis = 1)
+data_change_first_last['customer_ID'] = data_change_first_last.index
+data_change_first_last = data_change_first_last.reset_index(drop = True)
+data_change_first_last.columns = ['D_39_change_first_last', 'customer_ID']
 
 ## Joind the to dataset
 data_out = pd.merge(data_avg, data_median, on = 'customer_ID', how = 'left')
 data_out = pd.merge(data_out, data_change, on = 'customer_ID', how = 'left')
+data_out = pd.merge(data_out, data_change_first_last, on = 'customer_ID', how = 'left')
 
 data_out.to_csv('Deliquency_Features.csv', index = False)
