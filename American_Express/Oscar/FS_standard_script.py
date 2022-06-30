277a0af4202178c8245e278a2624f71775d9bac8
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-06-30 16:16:32 +0000

    updating

--- American_Express/Oscar/FS_standard_script.py
+++ American_Express/Oscar/FS_standard_script.py
@@ -4,3 +4,6 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from Amex_Metric import amex_metric
 
+## Reading data-file 
+data = pd.read_csv('Delinquency_Features.csv')
+
