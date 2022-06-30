e17571e8109dadb6accfff8bf1538552d915b08d
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-06-30 16:44:43 +0000

    updating

--- American_Express/Oscar/Amex_Metric.py
+++ American_Express/Oscar/Amex_Metric.py
@@ -4,18 +4,18 @@ import pandas as pd
 def amex_metric(y_true: pd.DataFrame, y_pred: pd.DataFrame) -> float:
 
     def top_four_percent_captured(y_true: pd.DataFrame, y_pred: pd.DataFrame) -> float:
-        df = (pd.concat([y_true, y_pred], axis='columns')
-              .sort_values('prediction', ascending=False))
-        df['weight'] = df['target'].apply(lambda x: 20 if x==0 else 1)
+        df = (pd.concat([y_true, y_pred], axis = 'columns')
+              .sort_values('prediction', ascending = False))
+        df['weight'] = df['target'].apply(lambda x: 20 if x == 0 else 1)
         four_pct_cutoff = int(0.04 * df['weight'].sum())
         df['weight_cumsum'] = df['weight'].cumsum()
         df_cutoff = df.loc[df['weight_cumsum'] <= four_pct_cutoff]
         return (df_cutoff['target'] == 1).sum() / (df['target'] == 1).sum()
         
     def weighted_gini(y_true: pd.DataFrame, y_pred: pd.DataFrame) -> float:
-        df = (pd.concat([y_true, y_pred], axis='columns')
-              .sort_values('prediction', ascending=False))
-        df['weight'] = df['target'].apply(lambda x: 20 if x==0 else 1)
+        df = (pd.concat([y_true, y_pred], axis = 'columns')
+              .sort_values('prediction', ascending = False))
+        df['weight'] = df['target'].apply(lambda x: 20 if x == 0 else 1)
         df['random'] = (df['weight'] / df['weight'].sum()).cumsum()
         total_pos = (df['target'] * df['weight']).sum()
         df['cum_pos_found'] = (df['target'] * df['weight']).cumsum()
@@ -24,7 +24,7 @@ def amex_metric(y_true: pd.DataFrame, y_pred: pd.DataFrame) -> float:
         return df['gini'].sum()
 
     def normalized_weighted_gini(y_true: pd.DataFrame, y_pred: pd.DataFrame) -> float:
-        y_true_pred = y_true.rename(columns={'target': 'prediction'})
+        y_true_pred = y_true.rename(columns = {'target': 'prediction'})
         return weighted_gini(y_true, y_pred) / weighted_gini(y_true, y_true_pred)
 
     g = normalized_weighted_gini(y_true, y_pred)
