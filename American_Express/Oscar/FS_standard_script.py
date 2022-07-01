50deadf57913f8d7af205dff371936a368dd4cff
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-07-01 13:46:51 +0000

    adding make_scorer

--- American_Express/Oscar/FS_standard_script.py
+++ American_Express/Oscar/FS_standard_script.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import make_scorer
 from Amex_Metric import amex_metric
 
 ## Reading data-file 
