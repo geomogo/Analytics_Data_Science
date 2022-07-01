75efc207113a0546c034f986084eb9a826d89523
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-07-01 13:47:32 +0000

    updating libraries

--- American_Express/Oscar/FS_standard_script.py
+++ American_Express/Oscar/FS_standard_script.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import RFECV
 from sklearn.metrics import make_scorer
+from sklearn.ensemble import RandomForestClassifier
 from Amex_Metric import amex_metric
 
 ## Reading data-file 
