49f21577923ef3dc9ff066792d27b28ed8d8e598
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-07-01 13:52:17 +0000

    updating line 23

--- American_Express/Oscar/FS_standard_script.py
+++ American_Express/Oscar/FS_standard_script.py
@@ -20,6 +20,6 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, strat
 amex_function = make_scorer(amex_metric, greater_is_better = True, needs_proba = True)
 
 ## Running RFE with Random forest
-RF_auto_feature = RFECV(estimator = RandomForestClassifier(n_estimators = 100, max_depth = 3), step = 1, scoring = amex_function, min_features_to_select = 5, cv = 3).fit(X_train, Y_train)
+RF_auto_feature = RFECV(estimator = RandomForestClassifier(n_estimators = 300, max_depth = 3), step = 1, scoring = amex_function, min_features_to_select = 5, cv = 3).fit(X_train, Y_train)
 
 print(X_train.columns[RF_auto_feature.support_])
