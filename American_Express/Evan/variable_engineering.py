c30c13f379c144ca4ce56d148c98a011315f4100
Evan Callaghan <evan.callaghan@grandview.edu>
2022-07-02 23:02:09 +0000

    Implementing details for variable engineering

---
+++ American_Express/Evan/variable_engineering.py
@@ -0,0 +1,38 @@
+## Function for variable engineering at the customer level
+
+## Importing libraries
+import pandas as pd
+import numpy as np
+
+
+def create_var(data, variable, method, new_name):
+    
+    ## Calculating the mean
+    if (method == 'mean'):
+        
+        ## Grouping the data by customer_ID to obtain the mean values
+        temp = pd.DataFrame(data.groupby(['customer_ID'])[variable].mean()).reset_index(drop = False)
+        
+        ## Cleaning the resulting data-frame
+        temp.columns = ['customer_ID', new_name]
+        
+    ## Calculating the median
+    elif (method == 'median'):
+        
+        ## Grouping the data by customer_ID to obtain the mean values
+        temp = pd.DataFrame(data.groupby(['customer_ID'])[variable].median()).reset_index(drop = False)
+        
+        ## Cleaning the resulting data-frame
+        temp.columns = ['customer_ID', new_name]
+        
+    ## Calculating the sum
+    elif (method == 'sum'):
+        
+        ## Grouping the data by customer_ID to obtain the mean values
+        temp = pd.DataFrame(data.groupby(['customer_ID'])[variable].sum()).reset_index(drop = False)
+        
+        ## Cleaning the resulting data-frame
+        temp.columns = ['customer_ID', new_name]
+        
+        
+    return temp
