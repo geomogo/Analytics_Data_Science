bd9aff639b3f18b282b7905e4bbc7a11580a87e1
Evan Callaghan <evan.callaghan@grandview.edu>
2022-07-15 12:55:12 +0000

    Edits

--- American_Express/Evan/feature_selection.py
+++ American_Express/Evan/feature_selection.py
@@ -5,23 +5,16 @@ import pandas as pd
 import numpy as np
 from tqdm import tqdm
 from sklearn.feature_selection import RFECV
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import make_scorer
+from Amex_Metric import amex_metric
 
 ## -------------------------------------------
 
 ## Reading customer-level data with engineered Payment and Spend features
 train = pd.read_csv('/home/ec2-user/SageMaker/Analytics_Data_Science/American_Express/Evan/amex_train_payment_spend.csv')
 #test = pd.read_csv('/home/ec2-user/SageMaker/Analytics_Data_Science/American_Express/Evan/amex_test_payment_spend.csv')
-
-## -------------------------------------------
-
-## Cleaning the customer-level data-frames
-train = train.drop(columns = ["('customer_ID', '')"])
-train.columns = ['customer_ID', 'P_2_mean', 'P_2_median', 'P_2_sum', 'P_2_count', 'P_2_data_range', 'P_2_iqr', 'P_2_avg_pct_change', 'P_2_correlation', 'P_3_mean', 'P_3_median', 'P_3_sum', 'P_3_count', 'P_3_data_range', 'P_3_iqr', 'P_3_avg_pct_change', 'P_3_correlation', 'P_4_mean', 'P_4_median', 'P_4_sum', 'P_4_data_range', 'P_4_iqr', 'P_4_avg_pct_change', 'P_4_correlation', 'S_3_median', 'S_3_sum', 'S_3_data_range', 'S_3_iqr', 'S_3_avg_pct_change', 'S_3_correlation', 'S_5_data_range', 'S_5_iqr', 'S_5_avg_pct_change', 'S_5_correlation', 'S_6_mean', 'S_6_median', 'S_6_sum', 'S_6_mad', 'S_6_data_range', 'S_6_iqr', 'S_6_avg_pct_change', 'S_6_correlation', 'S_8_mean', 'S_8_median', 'S_8_sum', 'S_8_data_range', 'S_8_iqr', 'S_8_avg_pct_change', 'S_8_correlation', 'S_13_mean', 'S_13_sum', 'S_13_std', 'S_13_mad', 'S_13_data_range', 'S_13_iqr', 'S_13_avg_pct_change', 'S_13_correlation', 'S_25_mean', 'S_25_sum', 'S_25_std', 'S_25_mad', 'S_25_count', 'S_25_data_range', 'S_25_iqr', 'S_25_avg_pct_change', 'S_25_correlation', 'S_27_count', 'S_27_data_range', 'S_27_iqr', 'S_27_avg_pct_change', 'S_27_correlation', 'target']
-
-# train = train.dropna()
-# Need to drop missing values
 
 ## -------------------------------------------
 
@@ -59,3 +52,45 @@ output.to_csv('feature_selection_results.csv', index = False)
 ## Producing the 10 feature to select
 to_select = output.iloc[0:10, 0]
 to_select.to_csv('feature_selection_results_top_10.csv', index = False)
+
+
+
+
+
+
+
+
+# import pandas as pd
+# import numpy as np
+# from sklearn.model_selection import train_test_split, GridSearchCV
+# from sklearn.feature_selection import RFECV
+# from sklearn.metrics import make_scorer
+# from sklearn.ensemble import RandomForestClassifier
+# from Amex_Metric import amex_metric
+
+# ## Reading data-file 
+# data = pd.read_csv('Delinquency_Features.csv')
+
+# ## Defining input and target variables
+# X = data.drop(columns = ['customer_ID', 'target'], axis = 1)
+# Y = data['target']
+
+# ## Spliting the data into train, validation, and test
+# X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, stratify = Y)
+
+# ## Defining the customized scoring function 
+# amex_function = make_scorer(amex_metric, greater_is_better = True, needs_proba = True)
+
+# ## Defining list to store results
+# features_to_select = list()
+
+# for i in range(0, 10):
+    
+#     ## Running RFE with Random forest
+#     RF_auto_feature = RFECV(estimator = RandomForestClassifier(n_estimators = 300, max_depth = 3), step = 1, scoring = amex_function, min_features_to_select = 10, cv = 3).fit(X_train, Y_train)
+
+#     ## Appending results 
+#     features_to_select.append(X_train.columns[RF_auto_feature.support_])
+    
+# ## Putting results as data-frame
+# features_to_select = pd.DataFrame(features_to_select)
