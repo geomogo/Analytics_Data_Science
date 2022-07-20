3af079e7f063542d079cc30b4fa69e0f3075f945
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-07-20 16:50:17 +0000

    adding files to be read

--- American_Express/Oscar/knn_imputation_categorical_real.py
+++ American_Express/Oscar/knn_imputation_categorical_real.py
@@ -2,9 +2,25 @@ import boto3
 import pandas as pd 
 import numpy as np
 
+import os
+import sagemaker
+
+sess = sagemaker.Session()
+
 s3 = boto3.resource('s3')
 bucket_name = 'analytics-data-science-competitions'
 bucket = s3.Bucket(bucket_name)
 
+## Defining files names
+file_key_1 = 'AmericanExpress/Delinquency_Features_Imputed.csv'
+file_key_2 = 'AmericanExpress/Delinquency_Features_Categorical_Last.csv'
+
+bucket_object_1 = bucket.Object(file_key_1)
+file_object_1 = bucket_object_1.get()
+file_content_stream_1 = file_object_1.get('Body')
+
+bucket_object_2 = bucket.Object(file_key_2)
+file_object_2 = bucket_object_2.get()
+file_content_stream_2 = file_object_2.get('Body')
 
 
