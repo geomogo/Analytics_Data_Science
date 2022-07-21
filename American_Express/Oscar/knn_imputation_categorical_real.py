6b7751d1704fab78ee91b3fb5df3702c02106266
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-07-21 14:30:15 +0000

    updating lines 39 and 42

--- American_Express/Oscar/knn_imputation_categorical_real.py
+++ American_Express/Oscar/knn_imputation_categorical_real.py
@@ -36,10 +36,10 @@ data_right = pd.read_csv(file_content_stream_2)
 data = pd.merge(data_left, data_right, on = 'customer_ID', how = 'left')
 
 ## Defining target features
-target = ['D_66_last', 'D_68_last', 'D_114_last', 'D_116_last', 'D_117_last', 'D_120_last']
+target = ['D_68_last', 'D_114_last', 'D_116_last', 'D_117_last', 'D_120_last']
 
 ## Defining input variables 
-X = data.drop(columns = ['customer_ID', 'target', 'D_63_last', 'D_64_last', 'D_66_last', 'D_68_last', 'D_114_last', 'D_116_last', 'D_117_last', 'D_120_last', 'D_120_last'], axis = 1)
+X = data.drop(columns = ['customer_ID', 'target', 'D_63_last', 'D_64_last', 'D_66_last', 'D_68_last', 'D_114_last', 'D_116_last', 'D_117_last', 'D_120_last', 'D_126_last'], axis = 1)
 
 scaler = MinMaxScaler()
 X = pd.DataFrame(scaler.fit_transform(X))
