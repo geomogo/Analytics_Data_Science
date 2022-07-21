d826345457b912460b66dd31a0508c8fd875a9e8
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-07-21 21:19:35 +0000

    updating reading data from s3 section

--- American_Express/Oscar/FS_standard_script.py
+++ American_Express/Oscar/FS_standard_script.py
@@ -7,8 +7,37 @@ from sklearn.metrics import make_scorer
 from sklearn.ensemble import RandomForestClassifier
 from Amex_Metric import amex_metric
 
-## Reading data-file 
-data = pd.read_csv('Delinquency_Features.csv')
+s3 = boto3.resource('s3')
+bucket_name = 'analytics-data-science-competitions'
+bucket = s3.Bucket(bucket_name)
+
+## Defining files names
+file_key = 'AmericanExpress/Delinquency_Features_Filled.csv'
+
+bucket_object = bucket.Object(file_key)
+file_object = bucket_object.get()
+file_content_stream = file_object.get('Body')
+
+## Reading data-files
+data = pd.read_csv(file_content_stream)
+data = data.drop(columns = ['D_64_last'], axis = 1)
+
+## Putting variables in the right shape 
+data['D_68_last'] = data['D_68_last'].astype(str)
+data['D_114_last'] = data['D_114_last'].astype(str)
+data['D_116_last'] = data['D_116_last'].astype(str)
+data['D_117_last'] = data['D_117_last'].astype(str)
+data['D_120_last'] = data['D_120_last'].astype(str)
+data['D_126_last'] = data['D_126_last'].astype(str)
+
+## Converting to dummies
+dummies = pd.get_dummies(data[['D_63_last', 'D_68_last', 'D_114_last', 'D_116_last', 'D_117_last', 'D_120_last', 'D_126_last']])
+
+## Appeding dummies 
+data = data.drop(columns = ['D_63_last', 'D_68_last', 'D_114_last', 'D_116_last', 'D_117_last', 'D_120_last', 'D_126_last'], axis = 1)
+data = pd.concat([data, dummies], axis = 1)
+
+
 
 ## Defining input and target variables
 X = data.drop(columns = ['customer_ID', 'target'], axis = 1)
