c9d45ccac602e51dde615f6bac879daaafe4f182
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-07-21 20:54:17 +0000

    adding boto3 library

--- American_Express/Oscar/FS_standard_script.py
+++ American_Express/Oscar/FS_standard_script.py
@@ -1,6 +1,7 @@
+import boto3
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import RFECV
 from sklearn.metrics import make_scorer
 from sklearn.ensemble import RandomForestClassifier
