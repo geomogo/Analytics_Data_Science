a1a9fa472565a514b19dd3ec10f10307ebb7ab70
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-07-22 01:01:04 +0000

    updating script

--- American_Express/Oscar/FS_standard_script.py
+++ American_Express/Oscar/FS_standard_script.py
@@ -43,7 +43,7 @@ X = data.drop(columns = ['customer_ID', 'target'], axis = 1)
 Y = data['target']
 
 ## Spliting the data into train, validation, and test
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.80, stratify = Y)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.70, stratify = Y)
 
 ## Defining the customized scoring function 
 amex_function = make_scorer(amex_metric, greater_is_better = True, needs_proba = True)
@@ -54,11 +54,11 @@ features_to_select = list()
 for i in tqdm(range(0, 10)):
     
     ## Running RFE with Random forest
-    RF_auto_feature = RFECV(estimator = RandomForestClassifier(n_estimators = 50, max_depth = 3), step = 30, scoring = amex_function, min_features_to_select = 10, cv = 3, n_jobs = -1).fit(X_train, Y_train)
+    RF_auto_feature = RFECV(estimator = RandomForestClassifier(n_estimators = 50, max_depth = 3), step = 40, scoring = amex_function, min_features_to_select = 10, cv = 3, n_jobs = -1).fit(X_train, Y_train)
 
     ## Appending results 
     features_to_select.append(X_train.columns[RF_auto_feature.support_])
     
 ## Putting results as data-frame
 features_to_select = pd.DataFrame(features_to_select)
-features_to_select.to_csv('Delinquency_Features_to_select_3.csv', index = False)
+features_to_select.to_csv('Delinquency_Features_to_select_4.csv', index = False)
