c17759c215e887ace769228990f282960bdf440d
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-07-23 14:40:02 +0000

    updating script

--- American_Express/Oscar/XGB_pred.py
+++ American_Express/Oscar/XGB_pred.py
@@ -1,3 +1,24 @@
 import boto3
 import pandas as pd
 import numpy as np
+import optuna
+from xgboost import XGBRegressor
+
+import os
+import sagemaker
+
+sess = sagemaker.Session()
+
+s3 = boto3.resource('s3')
+bucket_name = 'analytics-data-science-competitions'
+bucket = s3.Bucket(bucket_name)
+
+## Defining files names
+file_key = 'AmericanExpress/Delinquency_Features_Filled.csv'
+
+bucket_object = bucket.Object(file_key)
+file_object = bucket_object.get()
+file_content_stream = file_object.get('Body')
+
+## Reading data-files
+data = pd.read_csv(file_content_stream, usecols = [])
