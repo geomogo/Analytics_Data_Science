5929f5c51597d98c6276e4afab2561742125f16c
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-07-25 13:59:06 +0000

    commenting line 5

--- American_Express/Oscar/XGB_pred.py
+++ American_Express/Oscar/XGB_pred.py
@@ -2,7 +2,7 @@ import boto3
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+# from sklearn.metrics import accuracy_score
 import optuna
 from xgboost import XGBClassifier
 from Amex_Metric import amex_metric
