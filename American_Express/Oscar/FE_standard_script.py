b36ef11cbd88bdfca409dae24c989726a50fba00
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-07-27 15:07:38 +0000

    another update based on R_21

--- American_Express/Oscar/FE_standard_script.py
+++ American_Express/Oscar/FE_standard_script.py
@@ -20,38 +20,38 @@ file_content_stream_1 = file_object_1.get('Body')
 
 ## Creating data-type dictionary for reading the train data-frame
 # dtype_dict = {'customer_ID': "object", 'S_2': "object", 'P_2': 'float16', 'D_39': 'float16', 'B_1': 'float16','B_2': 'float16',
-#               'R_20': 'float16','S_3': 'float16','D_41': 'float16','B_3': 'float16','D_42': 'float16','D_43': 'float16','D_44': 'float16',
-#               'B_4': 'float16','D_45': 'float16','B_5': 'float16','R_20': 'float16','D_46': 'float16','D_47': 'float16','D_48': 'float16',
+#               'R_21': 'float16','S_3': 'float16','D_41': 'float16','B_3': 'float16','D_42': 'float16','D_43': 'float16','D_44': 'float16',
+#               'B_4': 'float16','D_45': 'float16','B_5': 'float16','R_21': 'float16','D_46': 'float16','D_47': 'float16','D_48': 'float16',
 #               'D_49': 'float16','B_6': 'float16','B_7': 'float16','B_8': 'float16','D_50': 'float16','D_51': 'float16','B_9': 'float16',
-#               'R_20': 'float16','D_52': 'float16','P_3': 'float16','B_10': 'float16','D_53': 'float16','S_5': 'float16','B_11': 'float16',
-#               'S_6': 'float16','D_54': 'float16','R_20': 'float16','S_7': 'float16','B_12': 'float16','S_8': 'float16','D_55': 'float16',
-#               'D_56': 'float16','B_13': 'float16','R_20': 'float16','D_58': 'float16','S_9': 'float16','B_14': 'float16','D_59': 'float16',
+#               'R_21': 'float16','D_52': 'float16','P_3': 'float16','B_10': 'float16','D_53': 'float16','S_5': 'float16','B_11': 'float16',
+#               'S_6': 'float16','D_54': 'float16','R_21': 'float16','S_7': 'float16','B_12': 'float16','S_8': 'float16','D_55': 'float16',
+#               'D_56': 'float16','B_13': 'float16','R_21': 'float16','D_58': 'float16','S_9': 'float16','B_14': 'float16','D_59': 'float16',
 #               'D_60': 'float16','D_61': 'float16','B_15': 'float16','S_11': 'float16','D_62': 'float16','D_63': 'object','D_64': 'object',
 #               'D_65': 'float16','B_16': 'float16','B_17': 'float16','B_18': 'float16','B_19': 'float16','D_66': 'float16','B_20': 'float16',
-#               'D_68': 'float16','S_12': 'float16','R_20': 'float16','S_13': 'float16','B_21': 'float16','D_69': 'float16','B_22': 'float16',
+#               'D_68': 'float16','S_12': 'float16','R_21': 'float16','S_13': 'float16','B_21': 'float16','D_69': 'float16','B_22': 'float16',
 #               'D_70': 'float16','D_71': 'float16','D_72': 'float16','S_15': 'float16','B_23': 'float16','D_73': 'float16','P_4': 'float16',
-#               'D_74': 'float16','D_75': 'float16','D_76': 'float16','B_24': 'float16','R_20': 'float16','D_77': 'float16','B_25': 'float16',
-#               'B_26': 'float16','D_78': 'float16','D_79': 'float16','R_20': 'float16','R_9': 'float16','S_16': 'float16','D_80': 'float16',
-#               'R_200': 'float16','R_201': 'float16','B_27': 'float16','D_81': 'float16','D_82': 'float16','S_17': 'float16','R_202': 'float16',
-#               'B_28': 'float16','R_203': 'float16','D_83': 'float16','R_204': 'float16','R_205': 'float16','D_84': 'float16','R_206': 'float16',
-#               'B_29': 'float16','B_30': 'float16','S_18': 'float16','D_86': 'float16','D_87': 'float16','R_207': 'float16','R_208': 'float16',
-#               'D_88': 'float16','B_31': 'int64','S_19': 'float16','R_209': 'float16','B_32': 'float16','S_20': 'float16','R_200': 'float16',
-#               'R_201': 'float16','B_33': 'float16','D_89': 'float16','R_202': 'float16','R_203': 'float16','D_91': 'float16','D_92': 'float16',
-#               'D_93': 'float16','D_94': 'float16','R_204': 'float16','R_205': 'float16','D_96': 'float16','S_22': 'float16','S_23': 'float16',
+#               'D_74': 'float16','D_75': 'float16','D_76': 'float16','B_24': 'float16','R_21': 'float16','D_77': 'float16','B_25': 'float16',
+#               'B_26': 'float16','D_78': 'float16','D_79': 'float16','R_21': 'float16','R_9': 'float16','S_16': 'float16','D_80': 'float16',
+#               'R_210': 'float16','R_211': 'float16','B_27': 'float16','D_81': 'float16','D_82': 'float16','S_17': 'float16','R_212': 'float16',
+#               'B_28': 'float16','R_213': 'float16','D_83': 'float16','R_214': 'float16','R_215': 'float16','D_84': 'float16','R_216': 'float16',
+#               'B_29': 'float16','B_30': 'float16','S_18': 'float16','D_86': 'float16','D_87': 'float16','R_217': 'float16','R_218': 'float16',
+#               'D_88': 'float16','B_31': 'int64','S_19': 'float16','R_219': 'float16','B_32': 'float16','S_20': 'float16','R_210': 'float16',
+#               'R_211': 'float16','B_33': 'float16','D_89': 'float16','R_212': 'float16','R_213': 'float16','D_91': 'float16','D_92': 'float16',
+#               'D_93': 'float16','D_94': 'float16','R_214': 'float16','R_215': 'float16','D_96': 'float16','S_22': 'float16','S_23': 'float16',
 #               'S_24': 'float16','S_25': 'float16','S_26': 'float16','D_102': 'float16','D_103': 'float16','D_104': 'float16','D_105': 'float16',
-#               'D_106': 'float16','D_107': 'float16','B_36': 'float16','B_37': 'float16', 'R_206': 'float16','R_207': 'float16','B_38': 'float16',
+#               'D_106': 'float16','D_107': 'float16','B_36': 'float16','B_37': 'float16', 'R_216': 'float16','R_217': 'float16','B_38': 'float16',
 #               'D_108': 'float16','D_109': 'float16','D_110': 'float16','D_111': 'float16','B_39': 'float16','D_112': 'float16','B_40': 'float16',
 #               'S_27': 'float16','D_113': 'float16','D_114': 'float16','D_115': 'float16','D_116': 'float16','D_117': 'float16','D_118': 'float16',
 #               'D_119': 'float16','D_120': 'float16','D_121': 'float16','D_122': 'float16','D_123': 'float16','D_124': 'float16','D_125': 'float16',
 #               'D_126': 'float16','D_127': 'float16','D_128': 'float16','D_129': 'float16','B_41': 'float16','B_42': 'float16','D_130': 'float16',
-#               'D_131': 'float16','D_132': 'float16','D_133': 'float16','R_208': 'float16','D_134': 'float16','D_135': 'float16','D_136': 'float16',
+#               'D_131': 'float16','D_132': 'float16','D_133': 'float16','R_218': 'float16','D_134': 'float16','D_135': 'float16','D_136': 'float16',
 #               'D_137': 'float16','D_138': 'float16','D_139': 'float16','D_140': 'float16','D_141': 'float16','D_142': 'float16','D_143': 'float16',
 #               'D_144': 'float16','D_145': 'float16'}
 
-dtype_dict = {'customer_ID': 'object', 'R_20': 'float16'}
+dtype_dict = {'customer_ID': 'object', 'R_21': 'float16'}
 
 ## Reading data-files
-train = pd.read_csv(file_content_stream_1, dtype = dtype_dict, usecols = ['customer_ID', 'R_20'])
+train = pd.read_csv(file_content_stream_1, dtype = dtype_dict, usecols = ['customer_ID', 'R_21'])
 # target = pd.read_csv(file_content_stream_2)
 
 risk_features = pd.read_csv('Risk_Features.csv')
@@ -75,37 +75,37 @@ risk_features = pd.read_csv('Risk_Features.csv')
 def summary_stats(x):
     
     d = {}
-    d['R_20_mean'] = x['R_20'].mean()
-    d['R_20_median'] = x['R_20'].median()
-    d['R_20_min'] = x['R_20'].min()
-    d['R_20_max'] = x['R_20'].max()
-    d['R_20_range'] = np.where(x['R_20'].shape[0] == 1, 0, x['R_20'].max() - x['R_20'].min())
-    d['R_20_IQR'] = np.where(x['R_20'].shape[0] == 1, 0,np.percentile(x['R_20'], 75) - np.percentile(x['R_20'], 25))
-    d['R_20_std'] = np.where(x['R_20'].shape[0] == 1, 0, np.std(x['R_20'], ddof = 1))
-#     d['R_20_negative_count'] = np.sum(x['R_20'] < 0) 
-#     d['R_20_positive_count'] = np.sum(x['R_20'] > 0)
-    d['R_20_pct_values_above_mean'] = np.where(x['R_20'].shape[0] == 1, 0, np.sum(x['R_20'] > x['R_20'].mean())/x['R_20'].shape[0])
-#     d['R_20_avg_pct_change'] = np.where(x['R_20'].shape[0] == 1, 0, pd.Series(x['R_20'].to_list()).pct_change().mean())
-    d['R_20_last_value'] = x['R_20'].iloc[-1]
+    d['R_21_mean'] = x['R_21'].mean()
+    d['R_21_median'] = x['R_21'].median()
+    d['R_21_min'] = x['R_21'].min()
+    d['R_21_max'] = x['R_21'].max()
+    d['R_21_range'] = np.where(x['R_21'].shape[0] == 1, 0, x['R_21'].max() - x['R_21'].min())
+    d['R_21_IQR'] = np.where(x['R_21'].shape[0] == 1, 0,np.percentile(x['R_21'], 75) - np.percentile(x['R_21'], 25))
+    d['R_21_std'] = np.where(x['R_21'].shape[0] == 1, 0, np.std(x['R_21'], ddof = 1))
+#     d['R_21_negative_count'] = np.sum(x['R_21'] < 0) 
+#     d['R_21_positive_count'] = np.sum(x['R_21'] > 0)
+    d['R_21_pct_values_above_mean'] = np.where(x['R_21'].shape[0] == 1, 0, np.sum(x['R_21'] > x['R_21'].mean())/x['R_21'].shape[0])
+#     d['R_21_avg_pct_change'] = np.where(x['R_21'].shape[0] == 1, 0, pd.Series(x['R_21'].to_list()).pct_change().mean())
+    d['R_21_last_value'] = x['R_21'].iloc[-1]
     
-    return pd.Series(d, index = ['R_20_mean', 'R_20_median', 'R_20_min', 'R_20_max', 'R_20_range', 'R_20_IQR', 'R_20_std', 'R_20_pct_values_above_mean', 'R_20_last_value'])
+    return pd.Series(d, index = ['R_21_mean', 'R_21_median', 'R_21_min', 'R_21_max', 'R_21_range', 'R_21_IQR', 'R_21_std', 'R_21_pct_values_above_mean', 'R_21_last_value'])
 
 data_out = train.groupby('customer_ID').apply(summary_stats)
 data_out['customer_ID'] = data_out.index
 data_out = data_out.reset_index(drop = True)
 
 # ## Computing average change at the customer level
-# data_change = pd.DataFrame(train_deli.groupby(['customer_ID'])['R_20'].apply(lambda x: pd.Series(x.to_list()).pct_change().mean()))
+# data_change = pd.DataFrame(train_deli.groupby(['customer_ID'])['R_21'].apply(lambda x: pd.Series(x.to_list()).pct_change().mean()))
 # data_change['customer_ID'] = data_change.index
 # data_change = data_change.reset_index(drop = True)
-# data_change.columns = ['R_20_change', 'customer_ID']
+# data_change.columns = ['R_21_change', 'customer_ID']
 
 # ## Computing change from first to last month
-# data_change_first_last = pd.DataFrame(train_deli.groupby(['customer_ID'])['R_20'].apply(lambda x: pd.Series(x.iloc[[0, -1]].to_list()).pct_change())).unstack()
-# data_change_first_last = data_change_first_last.drop(columns = ('R_20', 0), axis = 1)
+# data_change_first_last = pd.DataFrame(train_deli.groupby(['customer_ID'])['R_21'].apply(lambda x: pd.Series(x.iloc[[0, -1]].to_list()).pct_change())).unstack()
+# data_change_first_last = data_change_first_last.drop(columns = ('R_21', 0), axis = 1)
 # data_change_first_last['customer_ID'] = data_change_first_last.index
 # data_change_first_last = data_change_first_last.reset_index(drop = True)
-# data_change_first_last.columns = ['R_20_change_first_last', 'customer_ID']
+# data_change_first_last.columns = ['R_21_change_first_last', 'customer_ID']
 
 ## Joining the to datasets
 # data_out = pd.merge(customer_target, data_out, on = 'customer_ID', how = 'left')
