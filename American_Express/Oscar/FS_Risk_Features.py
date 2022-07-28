004b4995d2a9c6a75d0abfef4e18a857d845302b
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-07-28 00:01:38 +0000

    fixing typo on lines 45 and 46

--- American_Express/Oscar/FS_Risk_Features.py
+++ American_Express/Oscar/FS_Risk_Features.py
@@ -41,3 +41,27 @@ risk_data = risk_data.drop(columns = D_27_features, axis = 1)
 ## Appending lables
 risk_data = pd.merge(risk_data, labels, on = 'customer_ID', how = 'left')
 
+## Defining input and target variables
+X = risk_data.drop(columns = ['customer_ID', 'target'], axis = 1)
+Y = risk_data['target']
+
+## Spliting the data into train, validation, and test
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.20, stratify = Y)
+
+## Defining the customized scoring function 
+amex_function = make_scorer(amex_metric, greater_is_better = True, needs_proba = True)
+
+## Defining list to store results
+features_to_select = list()
+
+for i in tqdm(range(0, 10)):
+    
+    ## Running RFE with Random forest
+    RF_auto_feature = RFECV(estimator = RandomForestClassifier(n_estimators = 50, max_depth = 3), step = 50, scoring = amex_function, min_features_to_select = 10, cv = 3, n_jobs = -1).fit(X_train, Y_train)
+
+    ## Appending results 
+    features_to_select.append(X_train.columns[RF_auto_feature.support_])
+    
+## Putting results as data-frame
+features_to_select = pd.DataFrame(features_to_select)
+features_to_select.to_csv('Risk_Features_to_select.csv', index = False)
