8cdc4a7dbcfa2af508c2613524990de7421e4dde
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-07-28 00:42:57 +0000

    updating typo in line 37

--- American_Express/Oscar/Risk_knn_imputation.py
+++ American_Express/Oscar/Risk_knn_imputation.py
@@ -35,7 +35,7 @@ risk_data = risk_data.drop(columns = D_27_features, axis = 1)
 
 ## Defining buckets of variables
 buckets = ['R_1', 'R_2', 'R_3', 'R_4', 'R_5', 'R_6', 'R_7', 'R_8', 'R_10', 'R_11', 'R_12',
-           'R_13', 'R_14', 'R_15', 'R_16', 'R17', 'R_18', 'R_19', 'R_20', 'R_21', 'R_22',
+           'R_13', 'R_14', 'R_15', 'R_16', 'R_17', 'R_18', 'R_19', 'R_20', 'R_21', 'R_22',
            'R_23', 'R_24', 'R_25', 'R_28']
 
 ## Extracting features names
