c2c3d2574efa316bfc94c263636d4764e9cbdca4
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-08-01 17:27:12 +0000

    adding base run script

---
+++ Tabular-Playground-Aug-2022/base_run.py
@@ -0,0 +1,19 @@
+import boto3
+import pandas as pd; pd.set_option('display.max_columns', 100)
+import numpy as np
+
+s3 = boto3.resource('s3')
+bucket_name = 'analytics-data-science-competitions'
+bucket = s3.Bucket(bucket_name)
+
+file_key_1 = 'Tabular-Playground-Aug-2022/train.csv'
+file_key_2 = 'Tabular-Playground-Aug-2022/test.csv'
+
+bucket_object_1 = bucket.Object(file_key_1)
+file_object_1 = bucket_object_1.get()
+file_content_stream_1 = file_object_1.get('Body')
+
+bucket_object_2 = bucket.Object(file_key_2)
+file_object_2 = bucket_object_2.get()
+file_content_stream_2 = file_object_2.get('Body')
+
