0873b31eaa286fa20695e6077ae2cce4c9d906a2
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-08-01 22:31:35 +0000

    adding lightGBM script

---
+++ Tabular-Playground-Aug-2022/LightGBM_run.py
@@ -0,0 +1,47 @@
+import boto3
+import pandas as pd; pd.set_option('display.max_columns', 100)
+import numpy as np
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import roc_auc_score
+from xgboost import XGBClassifier
+
+import os
+import sagemaker
+
+sess = sagemaker.Session()
+
+s3 = boto3.resource('s3')
+bucket_name = 'analytics-data-science-competitions'
+bucket = s3.Bucket(bucket_name)
+
+file_key_1 = 'Tabular-Playground-Aug-2022/train.csv'
+file_key_2 = 'Tabular-Playground-Aug-2022/test.csv'
+
+bucket_object_1 = bucket.Object(file_key_1)
+file_object_1 = bucket_object_1.get()
+file_content_stream_1 = file_object_1.get('Body')
+
+bucket_object_2 = bucket.Object(file_key_2)
+file_object_2 = bucket_object_2.get()
+file_content_stream_2 = file_object_2.get('Body')
+
+## Reading data-files
+train = pd.read_csv(file_content_stream_1)
+train = train.drop(columns = ['id'], axis = 1)
+
+test = pd.read_csv(file_content_stream_2)
+test_id = test['id']
+test = test.drop(columns = ['id'], axis = 1)
+
+## Changing labels to dummies
+train_dummies = pd.get_dummies(train[['attribute_0']])
+train = train.drop(columns = ['product_code', 'attribute_0', 'attribute_1'], axis = 1)
+train = pd.concat([train, train_dummies], axis = 1)
+
+test_dummies = pd.get_dummies(test[['attribute_0']])
+test = test.drop(columns = ['product_code', 'attribute_0', 'attribute_1'], axis = 1)
+test = pd.concat([test, test_dummies], axis = 1)
+
+## Defining input and target variables
+X = train.drop(columns = ['failure'], axis = 1)
+Y = train['failure']
