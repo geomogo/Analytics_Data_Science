6d1c92197bc85fc2272069c48354215961fd9c91
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-08-02 17:30:26 +0000

    adding KNNimputer

--- Tabular-Playground-Aug-2022/XGBoost_run.py
+++ Tabular-Playground-Aug-2022/XGBoost_run.py
@@ -3,6 +3,7 @@ import pandas as pd; pd.set_option('display.max_columns', 100)
 import numpy as np
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import roc_auc_score
+from sklearn.impute import KNNImputer
 from xgboost import XGBClassifier
 
 import os
