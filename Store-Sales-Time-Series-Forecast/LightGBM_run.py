399fa244413a79a72b53c1291c95f938dab46905
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-08-20 00:09:46 +0000

    fixing type in line 92

--- Store-Sales-Time-Series-Forecast/LightGBM_run.py
+++ Store-Sales-Time-Series-Forecast/LightGBM_run.py
@@ -2,7 +2,7 @@ import boto3
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import GridSearchCV
-from xgboost import XGBRegressor
+from lightgbm import LGBMRegressor
 
 s3 = boto3.resource('s3')
 bucket_name = 'analytics-data-science-competitions'
@@ -84,57 +84,33 @@ test_dummies_2 = pd.get_dummies(test['store_type'])
 test_dummies_3 = pd.get_dummies(test['cluster'])
 test = pd.concat([test.drop(columns = ['family', 'store_type', 'cluster'], axis = 1), test_dummies_1, test_dummies_2, test_dummies_3], axis = 1)
 
-# ## Defining list to store results
-# results = list()
-
-# for i in range(0, len(clusters)):
-    
-#     print('Working on cluster ', i+1, 'out of', len(clusters))
-    
-#     train_loop = train[train['cluster'] == clusters[i]]
-#     train_loop = train_loop.drop(columns = ['cluster'], axis = 1)
-#     train_dummies = pd.get_dummies(train_loop['family'])
-#     train_loop = pd.concat([train_loop.drop(columns = 'family', axis = 1), train_dummies], axis = 1)
-    
-#     test_loop = test[test['cluster'] == clusters[i]]
-#     test_ids = test_loop['id']
-#     test_loop = test_loop.drop(columns = ['cluster', 'id'], axis = 1)
-#     test_dummies = pd.get_dummies(test_loop['family'])
-#     test_loop = pd.concat([test_loop.drop(columns = 'family', axis = 1), test_dummies], axis = 1)
-
-#     X = train_loop.drop(columns = ['sales'], axis = 1)
-#     Y = train_loop['sales']
 X = train.drop(columns = ['sales'], axis = 1)
 Y = train['sales']
 
 ## Defining the hyper-parameter grid
-XGBoost_param_grid = {'n_estimators': [300],
-                      'max_depth': [5, 7],
-                      'min_child_weight': [5, 7, 10],
-                      'learning_rate': [0.01, 0.001],
-                      'gamma': [0.3, 0.1],
-                      'subsample': [0.8, 1],
-                      'colsample_bytree': [0.8, 1]}
+LightGBM_param_grid = {'n_estimators': [100, 300, 500],
+                       'learning_rate': [0.01, 0.1, 1],
+                       'max_depth': [5, 7, 10],
+                       'num_leaves': [2, 5, 10, 15],
+                       'min_data_in_leaf': [10, 20, 50],
+                       'min_gain_to_split': [1, 5, 10],
+                       'lambda_l1': [0, 1, 10, 100],
+                       'lambda_l2': [0, 1, 10, 100]
+                       }
 
 ## Performing grid search with 5 folds
-XGBoost_grid_search = GridSearchCV(XGBRegressor(), XGBoost_param_grid, cv = 5, scoring = 'neg_mean_squared_log_error', n_jobs = -1, verbose = 4).fit(X, Y)
+LightGBM_grid_search = GridSearchCV(LGBMRegressor(), LightGBM_param_grid, cv = 5, scoring = 'neg_mean_squared_log_error', n_jobs = -1, verbose = 4).fit(X, Y)
 
 ## Extracting the best score
-best_score = XGBoost_grid_search.best_score_
+best_score = LightGBM_grid_search.best_score_
 print('The best mean squared log error:', best_score)
 
 ## Extracting the best model
-XGBoost_md = XGBoost_grid_search.best_estimator_
+LightGBM_md = LightGBM_grid_search.best_estimator_
 
 ## Predicting on test with best xgboost model 
-xgb_pred = XGBoost_md.predict(test_loop)
+LightGBM_pred = LightGBM_md.predict(test)
     
 ## Defining data-frame to store results
-data_out = pd.DataFrame({'id': test_ids, 'sales': xgb_pred})
-    
-#     ## Appending results
-#     results.append(data_out)
-
-## Combining results as a single data-frame
-results = pd.DataFrame(results)
-results.to_csv('XGBoost_baseline_run.csv', index = False)
+data_out = pd.DataFrame({'id': test_ids, 'sales': LightGBM_pred})
+data_out.to_csv('LightGBM_baseline_run.csv', index = False)
